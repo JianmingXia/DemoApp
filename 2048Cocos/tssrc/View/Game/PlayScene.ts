1ffdcc0b45b260a7217a4852dbdecfdb8b6b62f8
Ryoma <xiajianming@quqi.com>
2018-05-20 13:05:07 +0800

    merge card add valid direction

--- 2048Cocos/tssrc/View/Game/PlayScene.ts
+++ 2048Cocos/tssrc/View/Game/PlayScene.ts
@@ -108,9 +108,10 @@ export default class PlayScene extends NireusScene {
     private _onTouchEnded(touch: cc.Touch, event: cc.Event) {
         let direction = this._getTouchDirection(touch.getLocation());
 
-        // Valid Direction
-        this._slideCard(direction);
-        this._autoFillCardNumber();
+        if (this._isValidDirec(direction)) {
+            this._slideCard(direction);
+            this._autoFillCardNumber();
+        }
     }
 
     private _getTouchDirection(end_postion: cc.Point) {
@@ -137,74 +138,247 @@ export default class PlayScene extends NireusScene {
     }
 
     private _slideCard(direction: Direction) {
+        let now_number_arr = this._getNowNumber();
+
         switch (direction) {
             case Direction.UP:
-                this._slideCardUP();
-                break;
+                {
+                    const new_up_number_arr = this._mergeNum(this._transToCardUP(now_number_arr));
+
+                    this._resetCardNumber(now_number_arr, this._transFromCardUP(new_up_number_arr));
+                    break;
+                }
+            case Direction.DOWN:
+                {
+                    const new_down_number_arr = this._mergeNum(this._transToCardDown(now_number_arr));
+
+                    this._resetCardNumber(now_number_arr, this._transFromCardDown(new_down_number_arr));
+                    break;
+                }
+            case Direction.RIGHT:
+                {
+                    const new_right_number_arr = this._mergeNum(this._transToCardRight(now_number_arr));
+
+                    this._resetCardNumber(now_number_arr, this._transFromCardRight(new_right_number_arr));
+                    break;
+                }
+            case Direction.LEFT:
+                {
+                    const new_left_number_arr = this._mergeNum(now_number_arr);
+
+                    this._resetCardNumber(now_number_arr, new_left_number_arr);
+                    break;
+                }
         }
     }
 
-    private _slideCardUP() {
-        let have_num_count_arr = this._alignCardUP();
-
-        for (let col = 0; col < CARD_ROW_NUM; col++) {
-            if (have_num_count_arr[col] < 2) {
-                continue;
-            } else {
-                let num_arr: number[] = [];
-                for (let i = 0; i < have_num_count_arr[col]; i++) {
-                    num_arr[i] = this._card_arr[CARD_ROW_NUM - i - 1][col].number;
-                }
-
-                num_arr = this._mergeNum(num_arr);
-
-                if (num_arr.length !== have_num_count_arr[col]) {
-                    for (let i = 0; i < have_num_count_arr[col]; i++) {
-                        if (i < num_arr.length) {
-                            this._card_arr[CARD_ROW_NUM - i - 1][col].number = num_arr[i];
-                        } else {
-                            this._card_arr[CARD_ROW_NUM - i - 1][col].number = 0;
-                        }
-                    }
-                }
+    private _transToCardUP(now_number_arr: number[][]) {
+        let trans_number_arr: number[][] = [];
+        for (let i = 0; i < CARD_ROW_NUM; i++) {
+            trans_number_arr[i] = [];
+            for (let j = 0; j < CARD_ROW_NUM; j++) {
+                trans_number_arr[i].push(now_number_arr[CARD_ROW_NUM - j - 1][i]);
             }
         }
+
+        return trans_number_arr;
     }
 
-    private _mergeNum(num_arr: number[]) {
-        let len = num_arr.length;
+    private _transFromCardUP(now_number_arr: number[][]) {
+        let normal_num_arr: number[][] = [];
+        for (let i = 0; i < CARD_ROW_NUM; i++) {
+            normal_num_arr[i] = [];
+            for (let j = 0; j < CARD_ROW_NUM; j++) {
+                normal_num_arr[i].push(now_number_arr[j][CARD_ROW_NUM - i - 1]);
+            }
+        }
 
-        let new_num_arr: number[] = [];
-        for (let i = 0; i < len; i++) {
-            if (i !== len - 1 && num_arr[i] === num_arr[i + 1]) {
-                new_num_arr.push(2 * num_arr[i]);
-                i++;
-            } else {
-                new_num_arr.push(num_arr[i]);
+        return normal_num_arr;
+    }
+
+    private _transToCardDown(now_number_arr: number[][]) {
+        let trans_number_arr: number[][] = [];
+        for (let i = 0; i < CARD_ROW_NUM; i++) {
+            trans_number_arr[i] = [];
+            for (let j = 0; j < CARD_ROW_NUM; j++) {
+                trans_number_arr[i].push(now_number_arr[j][i]);
+            }
+        }
+
+        return trans_number_arr;
+    }
+
+    private _transFromCardDown(now_number_arr: number[][]) {
+        let normal_num_arr: number[][] = [];
+        for (let i = 0; i < CARD_ROW_NUM; i++) {
+            normal_num_arr[i] = [];
+            for (let j = 0; j < CARD_ROW_NUM; j++) {
+                normal_num_arr[i].push(now_number_arr[j][i]);
+            }
+        }
+
+        return normal_num_arr;
+    }
+
+    private _transToCardRight(now_number_arr: number[][]) {
+        let trans_number_arr: number[][] = [];
+        for (let i = 0; i < CARD_ROW_NUM; i++) {
+            trans_number_arr[i] = [];
+            for (let j = 0; j < CARD_ROW_NUM; j++) {
+                trans_number_arr[i].push(now_number_arr[i][CARD_ROW_NUM - j - 1]);
+            }
+        }
+
+        return trans_number_arr;
+    }
+
+    private _transFromCardRight(now_number_arr: number[][]) {
+        let normal_num_arr: number[][] = [];
+        for (let i = 0; i < CARD_ROW_NUM; i++) {
+            normal_num_arr[i] = [];
+            for (let j = 0; j < CARD_ROW_NUM; j++) {
+                normal_num_arr[i].push(now_number_arr[i][CARD_ROW_NUM - j - 1]);
+            }
+        }
+
+        return normal_num_arr;
+    }
+
+    private _mergeNum(num_arr: number[][]) {
+        let new_num_arr: number[][] = [];
+        for (let i = 0; i < CARD_ROW_NUM; i++) {
+            new_num_arr[i] = [];
+            let tmp_arr = num_arr[i].filter((value) => {
+                return value > 0;
+            });
+
+            for (let index = 0; index < tmp_arr.length; index++) {
+                if (index !== tmp_arr.length - 1 && tmp_arr[index] === tmp_arr[index + 1]) {
+                    new_num_arr[i].push(2 * tmp_arr[index]);
+                    index++;
+                } else {
+                    new_num_arr[i].push(tmp_arr[index]);
+                }
+            }
+
+            if (new_num_arr[i].length < num_arr[i].length) {
+                new_num_arr[i] = new_num_arr[i].concat(new Array(CARD_ROW_NUM - tmp_arr.length).fill(0));
             }
         }
 
         return new_num_arr;
     }
 
-    private _alignCardUP() {
-        let have_num_count_arr: number[] = [];
+    private _resetCardNumber(old_num_arr: number[][], new_num_arr: number[][]) {
+        for (let i = 0; i < CARD_ROW_NUM; i++) {
+            for (let j = 0; j < CARD_ROW_NUM; j++) {
+                if (old_num_arr[i][j] !== new_num_arr[i][j]) {
+                    this._card_arr[i][j].number = new_num_arr[i][j];
+                }
+            }
+        }
+    }
 
-        // realign
+    private _getNowNumber() {
+        let num_arr: number[][] = [];
+        for (let row = 0; row < CARD_ROW_NUM; row++) {
+            num_arr[row] = [];
+            for (let col = 0; col < CARD_ROW_NUM; col++) {
+                num_arr[row][col] = this._card_arr[row][col].number;
+            }
+        }
+
+        return num_arr;
+    }
+
+    private _isValidDirec(direction: Direction) {
+        switch (direction) {
+            case Direction.UP:
+                return this._isValidUpDirec();
+            case Direction.DOWN:
+                return this._isValidDownDirec();
+            case Direction.LEFT:
+                return this._isValidLeftDirec();
+            case Direction.RIGHT:
+                return this._isValidRightDirec();
+            default:
+                return true;
+        }
+    }
+
+    private _isValidUpDirec() {
+        let now_num_arr = this._getNowNumber();
         for (let col = 0; col < CARD_ROW_NUM; col++) {
-            have_num_count_arr[col] = 0;
+            let have_num_count = 0;
             for (let row = CARD_ROW_NUM - 1; row >= 0; row--) {
-                if (this._card_arr[row][col].number > 0) {
-                    have_num_count_arr[col]++;
-                    if (have_num_count_arr[col] + row !== CARD_ROW_NUM) {
-                        this._card_arr[CARD_ROW_NUM - have_num_count_arr[col]][col].number =
-                            this._card_arr[row][col].number;
-                        this._card_arr[row][col].number = 0;
+                if (now_num_arr[row][col] > 0) {
+                    have_num_count++;
+
+                    if (row !== CARD_ROW_NUM - 1 && (have_num_count + row < CARD_ROW_NUM ||
+                        this._card_arr[row][col].number === this._card_arr[row + 1][col].number)) {
+                        return true;
                     }
                 }
             }
         }
 
-        return have_num_count_arr;
+        return false;
+    }
+
+    private _isValidDownDirec() {
+        let now_num_arr = this._getNowNumber();
+        for (let col = 0; col < CARD_ROW_NUM; col++) {
+            let have_num_count = 0;
+            for (let row = 0; row < CARD_ROW_NUM; row++) {
+                if (now_num_arr[row][col] > 0) {
+                    have_num_count++;
+
+                    if (row !== 0 && (have_num_count - 1 !== row ||
+                        now_num_arr[row][col] === now_num_arr[row - 1][col])) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private _isValidLeftDirec() {
+        let now_num_arr = this._getNowNumber();
+        for (let row = 0; row < CARD_ROW_NUM; row++) {
+            let have_num_count = 0;
+            for (let col = 0; col < CARD_ROW_NUM; col++) {
+                if (now_num_arr[row][col] > 0) {
+                    have_num_count++;
+
+                    if (col !== 0 && (have_num_count - 1 !== col ||
+                        now_num_arr[row][col] === now_num_arr[row][col - 1])) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private _isValidRightDirec() {
+        let now_num_arr = this._getNowNumber();
+        for (let row = 0; row < CARD_ROW_NUM; row++) {
+            let have_num_count = 0;
+            for (let col = CARD_ROW_NUM - 1; col >= 0; col--) {
+                if (now_num_arr[row][col] > 0) {
+                    have_num_count++;
+
+                    if (col !== CARD_ROW_NUM - 1 && (have_num_count + col !== CARD_ROW_NUM ||
+                        now_num_arr[row][col] === now_num_arr[row][col + 1])) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 }
